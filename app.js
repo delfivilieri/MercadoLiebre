4eec6b34eb5cc84d75861e401f619d7721457a74
delfivilieri <110919394+delfivilieri@users.noreply.github.com>
2022-09-06 23:25:27 -0300

    Mejorando views

--- app.js
+++ app.js
@@ -1,95 +1,22 @@
-const express = require('express'); // por convención se llama express
-const app= express() 
-console.log(app);
+const express = require('express')
+const app = express()
+const path = require('path')
 
-app.listen(3000, () => console.log('Esto fue exitoso')); 
-const path = require('path');
-app.use('/static', express.static(path.join(__dirname, '/public')));
+app.listen(process.env.PORT || 3032, function(){
+    console.log('Servidor activo')
+})
 
+app.use(express.static(path.join(__dirname , './public')))
 
-
-app.get('/', (req,res)=>{
-    let htmlPath= path.resolve(__dirname, './views/home.html')
-    res.sendFile(htmlPath)
-    
-}
-)
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname , './views/home.html'))
+})
 
 
-app.get('/style.css', (req,res)=>{
-    let htmlPath= path.resolve(__dirname, './public/css.css')
-    res.sendFile(htmlPath)
-    
-}
-)
+app.get('/login', function(req, res){
+    res.sendFile(path.join(__dirname , './views/login.html'))
+})
 
-app.get('/login.css', (req,res)=>{
-    let htmlPath= path.resolve(__dirname, './public/login.css')
-    res.sendFile(htmlPath)
-    
-}
-)
-
-app.get('/register.css', (req,res)=>{
-    let htmlPath= path.resolve(__dirname, './public/register.css')
-    res.sendFile(htmlPath)
-    
-}
-)
-
-
-app.get('/moulinex.jpg', (req,res)=>{
-    let htmlPath= path.resolve(__dirname, './public/images/img-cafetera-moulinex.jpg') 
-    res.sendFile(htmlPath)
-    
-}
-)
-
-app.get('/banner.jpg', (req,res)=>{
-    let htmlPath= path.resolve(__dirname, './public/images/img-home-banner.jpg')  
-    res.sendFile(htmlPath)
-    
-}
-)
-
-app.get('/macbook.jpg', (req,res)=>{
-    let htmlPath= path.resolve(__dirname, './public/images/img-macbook-pro-2019.jpg')  
-    res.sendFile(htmlPath)
-    
-}
-)
-
-app.get('/samsung.jpg', (req,res)=>{
-    let htmlPath= path.resolve(__dirname, './public/images/img-samsung-galaxy-s10.jpg')  
-    res.sendFile(htmlPath)
-    
-}
-)
-
-app.get('/tv.jpg', (req,res)=>{
-    let htmlPath= path.resolve(__dirname, './public/images/img-tv-samsung-smart.jpg') 
-    res.sendFile(htmlPath)
-    
-}
-)
-
-app.get('/logo.jpg', (req,res)=>{
-    let htmlPath= path.resolve(__dirname, './public/images/logo-mercado-liebre.svg') 
-    res.sendFile(htmlPath)
-    
-}
-)
-
-app.get('/login', (req,res)=>{
-    let htmlPath= path.resolve(__dirname, './views/login.html')
-    res.sendFile(htmlPath)
-    
-}
-)
-
-app.get('/register', (req,res)=>{
-    let htmlPath= path.resolve(__dirname, './views/register.html')
-    res.sendFile(htmlPath)
-    
-}
-)
+app.get('/register', function(req, res){
+    res.sendFile(path.join(__dirname , './views/register.html'))
+})
